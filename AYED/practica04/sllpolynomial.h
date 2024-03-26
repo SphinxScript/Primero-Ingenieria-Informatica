4a7d3a1f6e51993f90730ad7f8b93ef52c29af90
SphynxScript <alu0101643137@gmail.com>
2024-03-26 20:39:31 +0000

    last p4

--- AYED/practica04/sllpolynomial.h
+++ AYED/practica04/sllpolynomial.h
@@ -50,8 +50,13 @@ bool IsNotZero(const double val, const double eps = EPS) {
 // FASE II
 // constructor
 SllPolynomial::SllPolynomial(const vector_t<double>& v, const double eps) {
-  // poner el código aquí
-  
+  for (int i{v.get_size() - 1}; i >= 0; --i) {
+    if (IsNotZero(v.get_val(i), eps)) {
+      pair_double_t monomio(v.get_val(i), i);
+      SllPolyNode* nodo{new SllPolyNode(monomio)};
+      push_front(nodo);
+    }
+  }
 }
 
 
