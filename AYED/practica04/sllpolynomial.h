3e3b718685a9c2704f415eba0830311b095c7d28
SphynxScript <alu0101643137@gmail.com>
2024-03-30 13:08:39 +0000

    last

--- AYED/practica04/sllpolynomial.h
+++ AYED/practica04/sllpolynomial.h
@@ -93,27 +93,58 @@ std::ostream& operator<<(std::ostream& os, const SllPolynomial& p) {
 // Evaluación de un polinomio representado por lista simple
 double SllPolynomial::Eval(const double x) const {
   double result{0.0};
-  // poner el código aquí
-  
+  SllPolyNode* aux = get_head();
+  while (aux != NULL) {
+    result += aux->get_data().get_val() * pow(x, aux->get_data().get_inx());
+    aux = aux->get_next();
+  }
   return result;
 }
 
 // Comparación si son iguales dos polinomios representados por listas simples
-bool SllPolynomial::IsEqual(const SllPolynomial& sllpol,
-			    const double eps) const {
-  bool differents = false;
-  // poner el código aquí
-
-  return !differents;
+bool SllPolynomial::IsEqual(const SllPolynomial& sllpol, const double eps) const {
+  SllPolyNode* aux = get_head();
+  SllPolyNode* aux2 = sllpol.get_head();
+  while (aux != NULL && aux2 != NULL) {
+    // Si los coeficientes son diferentes ó los índices son diferentes, los polinomios no son iguales
+    if (fabs(aux->get_data().get_val() - aux2->get_data().get_val()) > eps || aux->get_data().get_inx() != aux2->get_data().get_inx()) {
+      return false;
+    }
+    aux = aux->get_next();
+    aux2 = aux2->get_next();
+  }
+  // Si uno de los polinomios todavía tiene términos mientras el otro no, no son iguales
+  return aux == NULL && aux2 == NULL;
 }
 
 // FASE IV
 // Generar nuevo polinomio suma del polinomio invocante mas otro polinomio
-void SllPolynomial::Sum(const SllPolynomial& sllpol,
-			SllPolynomial& sllpolsum,
-			const double eps) {
-  // poner el código aquí
-
+void SllPolynomial::Sum(const SllPolynomial& sllpol, SllPolynomial& sllpolsum, const double eps) {
+  SllPolyNode* aux = get_head();
+  SllPolyNode* aux2 = sllpol.get_head();
+  double coef{0};
+  int inx{0};
+  while(aux != NULL || aux2 != NULL) {
+    if(aux != NULL && (aux2 == NULL || aux->get_data().get_inx() > aux2->get_data().get_inx())) {
+      coef = aux->get_data().get_val();
+      inx = aux->get_data().get_inx();
+      aux = aux->get_next();
+    }
+    else if(aux2 != NULL && (aux == NULL || aux2->get_data().get_inx() > aux->get_data().get_inx())) {
+      coef = aux2->get_data().get_val();
+      inx = aux2->get_data().get_inx();
+      aux2 = aux2->get_next();
+    }
+    else {
+      coef = aux->get_data().get_val() + aux2->get_data().get_val();
+      inx = aux->get_data().get_inx();
+      aux = aux->get_next();
+      aux2 = aux2->get_next();
+    }
+    if (fabs(coef) > eps) {
+      sllpolsum.push_front(new SllPolyNode(pair_double_t(coef, inx)));
+    }
+  }
 }
 
 
